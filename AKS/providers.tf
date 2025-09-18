f3fe7f69f3a3d02ce0d2fb82d1b4533b1664e6d6
behnam <b.hajian@gmail.com>
2025-09-18 12:17:02 -0400

    fixes providers

--- AKS/providers.tf
+++ AKS/providers.tf
@@ -1,17 +1,15 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
+required_providers {
+  azurerm = {
+    source  = "hashicorp/azurerm"
+    version = ">= 3.114.0"
+  }
+  random = {
+    source  = "hashicorp/random"
+    version = "~> 3.6"
   }
 }
 
 provider "azurerm" {
   features {}
+  use_cli = true
 }
